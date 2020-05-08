ab8ce5ce6318fbedcd3fc4af8ddbafcb6ea91d7e
TWei <twei@TWei-deMacBook-Pro.local>
2020-05-08 15:45:32 +0800

    [#1]Create Boards list fragment

--- app/src/main/java/com/example/selfquarantine/view/BoardsListFragment.kt
+++ app/src/main/java/com/example/selfquarantine/view/BoardsListFragment.kt
@@ -1,7 +1,25 @@
 package com.example.selfquarantine.view
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.selfquarantine.databinding.FragmentBoardsListBinding
 
 class BoardsListFragment : Fragment() {
-    
+
+    lateinit var binding : FragmentBoardsListBinding
+
+    override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle? ): View? {
+
+        binding = FragmentBoardsListBinding.inflate(inflater, container, false)
+        initBinding()
+        return binding.root
+    }
+
+    private fun initBinding(){
+        binding.handler = this
+        binding.lifecycleOwner=this
+    }
 }
